ab9c093146f0fe58f6802cfd15465772d8b5c213
Shreyash232 <46557265+Shreyash232@users.noreply.github.com>
2019-01-10 14:59:10 +0530

    Add files via upload
    10 jan 2019

---
+++ calculator.py
@@ -0,0 +1,24 @@
+a = float(input("Enter first number:"))
+b = float(input("Enter second number:"))
+
+# addition 
+result = a + b
+print(f"\nAddition of two number {result}\n")
+
+
+# substraction
+result = a-b
+print(f"\nsubstraction of two number is {result}\n")
+
+
+# division
+result =a/b
+print(f"\ndivision of two number is {result}\n")
+
+# multiplication
+result = a*b
+print(f"\nmultiplication of two number is {result}\n")
+
+# expodential 
+result =a**b
+print(f"\nexpodential of two number is {result}\n")
