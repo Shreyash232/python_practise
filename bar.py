265432a51975b4acabc236420a5780db9adc2b39
Shreyash232 <46557265+Shreyash232@users.noreply.github.com>
2019-01-11 22:48:13 +0530

    Add files via upload
    11jan2019

---
+++ bar.py
@@ -0,0 +1,10 @@
+a=int(input("whats your age:"))
+if a>18 and a<21:
+	print("you will get wrist band only")
+
+elif a>21:
+	print("you will get entry and drink")
+
+elif a<18 :
+	print("you are not allowed to go")
+   
